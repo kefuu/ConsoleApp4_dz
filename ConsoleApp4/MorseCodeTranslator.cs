c37cfb275d41bc03ecf6932d9a05c81d164a92b9
creep <creep@DESKTOP-CQ3GK11>
2023-05-17 14:04:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/MorseCodeTranslator.cs
@@ -0,0 +1,81 @@
+﻿namespace ConsoleApp4
+{
+    internal class MorseCodeTranslator
+    {
+        static Dictionary<char, string> morseAlphabetDictionary;
+
+        public static void MorseCodeTranslatorFunc(string[] args)
+        {
+            InitializeDictionary();
+
+            Console.Write("Enter text to convert to Morse code: ");
+            string input = Console.ReadLine().ToUpper();
+
+            string morseCode = TranslateToMorseCode(input);
+            Console.WriteLine($"Morse code: {morseCode}");
+        }
+
+        static void InitializeDictionary()
+        {
+            morseAlphabetDictionary = new Dictionary<char, string>()
+            {
+                { 'A', ".-" },
+                { 'B', "-..." },
+                { 'C', "-.-." },
+                { 'D', "-.." },
+                { 'E', "." },
+                { 'F', "..-." },
+                { 'G', "--." },
+                { 'H', "...." },
+                { 'I', ".." },
+                { 'J', ".---" },
+                { 'K', "-.-" },
+                { 'L', ".-.." },
+                { 'M', "--" },
+                { 'N', "-." },
+                { 'O', "---" },
+                { 'P', ".--." },
+                { 'Q', "--.-" },
+                { 'R', ".-." },
+                { 'S', "..." },
+                { 'T', "-" },
+                { 'U', "..-" },
+                { 'V', "...-" },
+                { 'W', ".--" },
+                { 'X', "-..-" },
+                { 'Y', "-.--" },
+                { 'Z', "--.." },
+                { '0', "-----" },
+                { '1', ".----" },
+                { '2', "..---" },
+                { '3', "...--" },
+                { '4', "....-" },
+                { '5', "....." },
+                { '6', "-...." },
+                { '7', "--..." },
+                { '8', "---.." },
+                { '9', "----." },
+                { ' ', "/" }
+            };
+        }
+
+        static string TranslateToMorseCode(string input)
+        {
+            List<string> morseCode = new List<string>(input.Length);
+
+            foreach (char character in input)
+            {
+                if (morseAlphabetDictionary.TryGetValue(character, out string morseCharacter))
+                {
+                    morseCode.Add(morseCharacter);
+                }
+                else
+                {
+                    Console.WriteLine($"Warning: Character '{character}' is not supported and will be skipped.");
+                }
+            }
+
+            return string.Join(" ", morseCode);
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe.TicTacToeFunc(args);
+            // MorseCodeTranslator.MorseCodeTranslatorFunc(args);
+        }
+    }
+}
